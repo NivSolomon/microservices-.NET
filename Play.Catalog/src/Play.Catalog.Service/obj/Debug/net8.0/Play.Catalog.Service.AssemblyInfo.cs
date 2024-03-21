b82d415dd3cebd24702e62cabae839a12af9772b
Niv Solomon <nivsolomon3@gmail.com>
2024-03-21 12:15:33 +0200

    build ItemsController

--- Play.Catalog/src/Play.Catalog.Service/obj/Debug/net8.0/Play.Catalog.Service.AssemblyInfo.cs
+++ Play.Catalog/src/Play.Catalog.Service/obj/Debug/net8.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d382ad17e9181bac1d629d0c08227728239868")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26f0c3de810ed83a8e5f316075c7c798f565ab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
