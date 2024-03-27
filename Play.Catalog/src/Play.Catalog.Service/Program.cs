fee358133f3a6d739381b98b446257007618a66f
Niv Solomon <nivsolomon3@gmail.com>
2024-03-27 11:40:03 +0200

    add singleton

--- Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,8 +1,12 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using Play.Catalog.Service.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
+
+private ServiceSettings serviceSettings;
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,6 +18,13 @@ builder.Services.AddSwaggerGen();
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
+var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+builder.Services.AddSingleton(serviceProvider =>
+{
+    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+    return mongoClient.GetDatabase(serviceSettings.ServiceName);
+});
 
 var app = builder.Build();
 app.MapControllers();
